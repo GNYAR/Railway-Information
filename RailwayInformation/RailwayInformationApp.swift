e874ea9e7358ece184f6b3fa0a2e83c1f0b87795
GNYAR <s901121s901121@gmail.com>
2022-11-16 15:03:45 +0800

    Initial Commit

---
+++ RailwayInformation/RailwayInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailwayInformationApp.swift
+//  RailwayInformation
+//
+//  Created by User20 on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct RailwayInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
