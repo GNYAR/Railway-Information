bb10441efc2f4c31a94a766032a79d26258f569e
GNYAR <s901121s901121@gmail.com>
2022-12-14 16:58:10 +0800

    refactor(member): use URLSession to initialize the header

--- RailwayInformation/API/MemberController.swift
+++ RailwayInformation/API/MemberController.swift
@@ -11,11 +11,22 @@ class MemberController: ObservableObject {
   @Published var showError = false
   @Published var loading = 0
   @Published var user: User? = nil
+  var session: URLSession = URLSession(configuration: .default)
+  
   let USERS_URL: String
   
   init() {
     let HTTPS_HOST = "https://favqs.com/api"
     USERS_URL = "\(HTTPS_HOST)/users"
+    
+    let contentType = "application/json"
+    let auth = "Token token=\(Bundle.main.infoDictionary?["FAVQS"] ?? "")"
+    let config = URLSessionConfiguration.default
+    config.httpAdditionalHeaders = [
+      "content-type": contentType,
+      "authorization": auth
+    ]
+    session = URLSession(configuration: config)
   }
   
   var error: Error? {
@@ -33,28 +44,22 @@ class MemberController: ObservableObject {
   
   func createUser(login: String, email: String, password: String) {
     loading += 1
-    let contentType = "application/json"
-    let auth = "Token token=\(Bundle.main.infoDictionary?["FAVQS"] ?? "")"
     let userInput = UserInput(user: UserLoginInput(login: login, email: email, password: password))
     let data = try? JSONEncoder().encode(userInput)
     
     guard let url = URL(string: USERS_URL) else { return }
     var request = URLRequest(url: url)
     request.httpMethod = "POST"
-    request.setValue(auth, forHTTPHeaderField: "authorization")
-    request.setValue(contentType, forHTTPHeaderField: "content-type")
     request.httpBody = data
     
-    URLSession.shared.dataTask(with: request) { data, response, error in
+    session.dataTask(with: request) { data, response, error in
       if let data = data {
         do {
-          print(data.description)
           let user = try JSONDecoder().decode(User.self, from: data)
           let userError = try JSONDecoder().decode(UserError.self, from: data)
           guard userError.errorCode == nil else { throw userError }
           DispatchQueue.main.sync {
             self.user = user
-            print(user.login ?? "nil")
             self.error = nil
             self.loading -= 1
           }
