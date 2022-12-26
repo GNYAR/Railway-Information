9e4c440a33bbc44c24cf7140cc8c3c347a43d8b8
GNYAR <s901121s901121@gmail.com>
2022-12-26 12:09:05 +0800

    style(search): correct the word to chinese

--- RailwayInformation/Components/Search.swift
+++ RailwayInformation/Components/Search.swift
@@ -176,9 +176,8 @@ struct SearchBar: View {
           self.text = ""
           UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
           
-        }) {
-          Text("Cancel")
-        }
+        }) { Text("取消") }
+        .padding(.vertical, 7)
         .padding(.trailing, 10)
         .transition(.move(edge: .trailing))
         .animation(.default)
